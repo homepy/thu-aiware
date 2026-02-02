521ce8898edc33dd0915d75b095e9450ed713f77
homepy <hopex40@163.com>
2026-02-03 03:41:38 +0800

    test: result of vllm bench serve

--- vllm/bench_result/bench4/test.sh
+++ vllm/bench_result/bench4/test.sh
@@ -1,41 +1,68 @@
-# Qwen3-8B-AWQ benchmark serving script
-vllm bench serve \
-    --dataset-name random \
-    --model "Qwen3-8B-AWQ" \
-    --tokenizer "Qwen/Qwen3-8B-AWQ" \
-    --num-prompts "100" \
-    --max-concurrency "2" \
-    --random-input-len "3072" \
-    --random-output-len "64" \
-    --trust-remote-code \
-    --ignore-eos \
-    --host 127.0.0.1 \
-    --port 8001 \
-    --save-result \
-    --extra-body '{"chat_template_kwargs":{"enable_thinking":false}}'
-
-# Qwen3-8B benchmark serving script
-vllm bench serve \
-    --dataset-name random \
-    --model "Qwen3-8B" \
-    --tokenizer "Qwen/Qwen3-8B" \
-    --num-prompts "100" \
-    --max-concurrency "2" \
-    --random-input-len "3072" \
-    --random-output-len "64" \
-    --trust-remote-code \
-    --ignore-eos \
-    --host 127.0.0.1 \
-    --port 8001 \
-    --save-result \
-    --extra-body '{"chat_template_kwargs":{"enable_thinking":false}}'
-
-# Qwen3-14B benchmark serving script
+#!/usr/bin/env bash
 vllm bench serve \
     --dataset-name random \
     --model "Qwen3-14B" \
     --tokenizer "Qwen/Qwen3-14B" \
-    --num-prompts "100" \
+    --num-prompts "640" \
+    --max-concurrency "32" \
+    --random-input-len "3072" \
+    --random-output-len "64" \
+    --trust-remote-code \
+    --ignore-eos \
+    --host 127.0.0.1 \
+    --port 8001 \
+    --save-result
+
+vllm bench serve \
+    --dataset-name random \
+    --model "Qwen3-14B" \
+    --tokenizer "Qwen/Qwen3-14B" \
+    --num-prompts "640" \
+    --max-concurrency "16" \
+    --random-input-len "3072" \
+    --random-output-len "64" \
+    --trust-remote-code \
+    --ignore-eos \
+    --host 127.0.0.1 \
+    --port 8001 \
+    --save-result
+
+
+vllm bench serve \
+    --dataset-name random \
+    --model "Qwen3-14B" \
+    --tokenizer "Qwen/Qwen3-14B" \
+    --num-prompts "640" \
+    --max-concurrency "8" \
+    --random-input-len "3072" \
+    --random-output-len "64" \
+    --trust-remote-code \
+    --ignore-eos \
+    --host 127.0.0.1 \
+    --port 8001 \
+    --save-result
+
+
+vllm bench serve \
+    --dataset-name random \
+    --model "Qwen3-14B" \
+    --tokenizer "Qwen/Qwen3-14B" \
+    --num-prompts "640" \
+    --max-concurrency "4" \
+    --random-input-len "3072" \
+    --random-output-len "64" \
+    --trust-remote-code \
+    --ignore-eos \
+    --host 127.0.0.1 \
+    --port 8001 \
+    --save-result
+
+
+vllm bench serve \
+    --dataset-name random \
+    --model "Qwen3-14B" \
+    --tokenizer "Qwen/Qwen3-14B" \
+    --num-prompts "640" \
     --max-concurrency "2" \
     --random-input-len "3072" \
     --random-output-len "64" \
@@ -45,29 +72,13 @@ vllm bench serve \
     --port 8001 \
     --save-result
 
-# Qwen3-14B-AWQ benchmark serving script
-vllm bench serve \
-    --dataset-name random \
-    --model "Qwen3-14B-AWQ" \
-    --tokenizer "Qwen/Qwen3-14B-AWQ" \
-    --num-prompts "100" \
-    --max-concurrency "2" \
-    --random-input-len "3072" \
-    --random-output-len "64" \
-    --trust-remote-code \
-    --ignore-eos \
-    --host 127.0.0.1 \
-    --port 8001 \
-    --save-result 
-
-
 
 vllm bench serve \
     --dataset-name random \
     --model "Qwen3-14B" \
     --tokenizer "Qwen/Qwen3-14B" \
     --num-prompts "640" \
-    --max-concurrency "16" \
+    --max-concurrency "1" \
     --random-input-len "3072" \
     --random-output-len "64" \
     --trust-remote-code \
